31d203fe69d94ff34d59fa6f86589fc88cfa0d9e
RoFz <4117458+RoFz@users.noreply.github.com>
2026-09-17 23:14:43 +0000

    feat(modern): revise Black Team roster
    - feat: select the requested role loadouts for Fox through Makarova
    - feat: trim the full-squad definition to the twelve-slot DRO lineup

--- bskulls-modern/addons/bskulls-modern/CfgGroups.hpp
+++ bskulls-modern/addons/bskulls-modern/CfgGroups.hpp
@@ -121,19 +121,19 @@ class cfgGroups {
                         position[] = {5,-5,0};
                         rank = "MAJOR";
                         side = 1;
-                        vehicle = "B_PTbskull_Veh_Unit_Fox_blackops_03";
+                        vehicle = "B_PTbskull_Veh_Unit_Fox_blackops_04";
                     };
                     class black3 {
                         position[] = {-5,-5,0};
                         rank = "CAPTAIN";
                         side = 1;
-                        vehicle = "B_PTbskull_Veh_Unit_Vega_blackops_01";
+                        vehicle = "B_PTbskull_Veh_Unit_Vega_blackops_03";
                     };
                     class black4 {
                         position[] = {10,-10,0};
                         rank = "SERGEANT";
                         side = 1;
-                        vehicle = "B_PTbskull_Veh_Unit_McKendrick_blackops_03";
+                        vehicle = "B_PTbskull_Veh_Unit_McKendrick_blackops_06";
                     };
                     class black5 {
                         position[] = {-10,-10,0};
@@ -145,55 +145,43 @@ class cfgGroups {
                         position[] = {15,-15,0};
                         rank = "SERGEANT";
                         side = 1;
-                        vehicle = "B_PTbskull_Veh_Unit_Frost_blackops_01";
+                        vehicle = "B_PTbskull_Veh_Unit_Frost_blackops_04";
                     };
                     class black7 {
                         position[] = {-15,-15,0};
                         rank = "CORPORAL";
                         side = 1;
-                        vehicle = "B_PTbskull_Veh_Unit_Hawkins_blackops_01";
+                        vehicle = "B_PTbskull_Veh_Unit_Hawkins_blackops_06";
                     };
                     class black8 {
                         position[] = {20,-20,0};
                         rank = "PRIVATE";
                         side = 1;
-                        vehicle = "B_PTbskull_Veh_Unit_Sykes_blackops_01";
+                        vehicle = "B_PTbskull_Veh_Unit_Sykes_blackops_02";
                     };
                     class black9 {
                         position[] = {-20,-20,0};
                         rank = "LIEUTENANT";
                         side = 1;
-                        vehicle = "B_PTbskull_Veh_Unit_Everett_blackops_01";
+                        vehicle = "B_PTbskull_Veh_Unit_Everett_blackops_04";
                     };
                     class black10 {
                         position[] = {25,-25,0};
                         rank = "SERGEANT";
                         side = 1;
-                        vehicle = "B_PTbskull_Veh_Unit_Taylor_blackops_01";
+                        vehicle = "B_PTbskull_Veh_Unit_Taylor_blackops_02";
                     };
                     class black11 {
                         position[] = {-25,-25,0};
                         rank = "CORPORAL";
                         side = 1;
-                        vehicle = "B_PTbskull_Veh_Unit_Liu_blackops_01";
+                        vehicle = "B_PTbskull_Veh_Unit_Liu_blackops_02";
                     };
                     class black12 {
                         position[] = {30,-30,0};
                         rank = "PRIVATE";
                         side = 1;
                         vehicle = "B_PTbskull_Veh_Unit_Makarova_blackops_01";
-                    };
-                    class black13 {
-                        position[] = {-30,-30,0};
-                        rank = "PRIVATE";
-                        side = 1;
-                        vehicle = "B_PTbskull_Veh_Unit_Elias_blackops_02";
-                    };
-                    class black14 {
-                        position[] = {35,-35,0};
-                        rank = "PRIVATE";
-                        side = 1;
-                        vehicle = "B_PTbskull_Veh_Unit_Martinez_blackops_02";
                     };
                 };
             };
